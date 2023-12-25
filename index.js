08ce408c3ad24bff1a052fe2f5cc18272af9c73f
KOGR <66009682+egor1221@users.noreply.github.com>
2023-12-25 17:02:03 +0300

    1111111

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ import { Title } from "./tile.js";
 const gameBoard = document.getElementById("game-cont");
 
 const grid = new Grid(gameBoard);
-<<<<<<< HEAD
-grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
-=======
-grid.getRandomEmptyCell.linkTitle(new Title(gameBoard));
->>>>>>> 89abffcfee6c858a74650ca6d121f230b007b9ed
+grid.getRandomEmptyCell().linkTitle(new Title(gameBoard));
